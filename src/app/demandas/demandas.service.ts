e30763d1808a7aea82a684b711d64d12e523c8a6
henriquenkt <henriquenkt@gmail.com>
2023-02-27 18:00:32 -0300

    master

--- src/app/demandas/demandas.service.ts
+++ src/app/demandas/demandas.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -8,13 +8,27 @@ import { Observable } from 'rxjs';
 export class DemandasService {
 
   constructor(private http: HttpClient) {}
+  headers = new HttpHeaders().set('Accept', '*/*')
+  .set('Accept-Encoding', 'gzip, deflate, br')
+  // .set('Accept-Language', 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7')
+  .set('Connection', 'keep-alive')
+  // .set('Content-Length', '67')
+  // .set('Content-Length', '67')
+  .set('Access-Control-Allow-Origin', 'origin-list')
+  .set('Content-Type', 'multipart/form-data');
+  baseUrl = "https://fastproject.senior.com.br/ajax/demanda/select.php";
 
-  headers = new HttpHeaders().set('Content-Type', 'application/json');
-  baseUrl = "https://fastproject.senior.com.br/ajax/demanda/select.php?idUsuarioLogado=2897&status=0&usuario=0&produto=99&tipo=1&filial=99";
+  parametros = new HttpParams()
+  .set('idUsuarioLogado','2897')
+  .set('status', '0')
+  .set('usuario', '0')
+  .set('produto', '99')
+  .set('tipo', '1')
+  .set('filial', '99')
 
   getDemandas(): Observable<any> {
 
-    var result = this.http.post(`${this.baseUrl}`, {}, {
+    var result = this.http.post(`${this.baseUrl}`, this.parametros.toString(), {
       headers: this.headers,
     });
     return result;
